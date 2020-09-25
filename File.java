762658ce563ec05288dbdc7683c0ec774b330db7
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-09-25 17:06:42 +0530

    usecase6

--- File.java
+++ File.java
@@ -3,12 +3,14 @@ public static final int present=2;
 public static final int Halfpresent=1;
 public static final int RPH=20;
 public static final int numdays=20;
+public static final int maxhours=100;
 public static void main(String[] args){
 int empH=0;
 int empW=0;
-int total=0;
+int total=0 , totalWD=0 ,totalEH=0;
 System.out.println("Welcome");
-for(int day=0;day<numdays;day++){
+while(totalEH<=maxhours && totalWD<numdays){
+totalWD++;
 int check=(int)Math.floor(Math.random()*10 % 3);
 switch (check)
 {
@@ -24,10 +26,11 @@ default:
 
 empH=0;
 }
-empW=empH*RPH;
-total +=empW;
-System.out.println("Emp Wage: " + empW);
+totalEH +=empH;
+System.out.println("Day: " + totalWD + " Emp Hr: "+empH);
 }
+total=totalEH*RPH;
+
 System.out.println("Total Emp Wage: " + total);
 }
 }
