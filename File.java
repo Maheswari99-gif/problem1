4a11ac8cb1e1b168eefd710cfa7670239b816bb7
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-09-25 16:55:33 +0530

    usecase5

--- File.java
+++ File.java
@@ -2,16 +2,18 @@ public class File{
 public static final int present=2;
 public static final int Halfpresent=1;
 public static final int RPH=20;
+public static final int numdays=20;
 public static void main(String[] args){
 int empH=0;
 int empW=0;
-int check=(int)Math.floor(Math.random()*10 % 3);
+int total=0;
 System.out.println("Welcome");
+for(int day=0;day<numdays;day++){
+int check=(int)Math.floor(Math.random()*10 % 3);
 switch (check)
 {
 case present:
 empH=8;
-System.out.println("Employee is Present");
 break;
 
 case Halfpresent:
@@ -21,9 +23,11 @@ break;
 default:
 
 empH=0;
-System.out.println("Employee is Absent");
 }
 empW=empH*RPH;
+total +=empW;
 System.out.println("Emp Wage: " + empW);
+}
+System.out.println("Total Emp Wage: " + total);
 }
 }
