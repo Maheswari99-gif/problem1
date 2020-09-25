f7d11cfcfdf42269896cf87d31d563c0b35ab238
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-09-25 15:51:14 +0530

    usecase1

--- File.java
+++ File.java
@@ -1,5 +1,11 @@
 public class File{
+public static final int present=1;
 public static void main(String[] args){
+double check=Math.floor(Math.random()*10 % 2);
 System.out.println("Welcome");
+if(check==present)
+System.out.println("Employee is Present");
+else
+System.out.println("Employee is Absent");
 }
 }
