acd2d165484642cd895a5f4eca96a6289fc0edc0
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-09-25 16:10:59 +0530

    usecase3

--- File.java
+++ File.java
@@ -1,15 +1,20 @@
 public class File{
-public static final int present=1;
+public static final int present=2;
+public static final int Halfpresent=1;
 public static final int RPH=20;
 public static void main(String[] args){
 int empH=0;
 int empW=0;
-double check=Math.floor(Math.random()*10 % 2);
+double check=Math.floor(Math.random()*10 % 3);
 System.out.println("Welcome");
 if(check==present)
 {
 empH=8;
 System.out.println("Employee is Present");
+}
+else if(check==Halfpresent)
+{
+empH=4;
 }
 else
 {
