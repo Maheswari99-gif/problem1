85f195c773fbff352387b3313a817309a10a4968
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-09-25 16:33:28 +0530

    usecase4

--- File.java
+++ File.java
@@ -5,19 +5,21 @@ public static final int RPH=20;
 public static void main(String[] args){
 int empH=0;
 int empW=0;
-double check=Math.floor(Math.random()*10 % 3);
+int check=(int)Math.floor(Math.random()*10 % 3);
 System.out.println("Welcome");
-if(check==present)
+switch (check)
 {
+case present:
 empH=8;
 System.out.println("Employee is Present");
-}
-else if(check==Halfpresent)
-{
+break;
+
+case Halfpresent:
 empH=4;
-}
-else
-{
+break;
+
+default:
+
 empH=0;
 System.out.println("Employee is Absent");
 }
