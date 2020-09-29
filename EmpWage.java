ce8252b15e0c45061da68faeff95fc680eed7463
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-09-29 12:29:23 +0530

    usecase8

--- EmpWage.java
+++ EmpWage.java
@@ -1,14 +1,11 @@
 public class EmpWage{
 public static final int IS_FULL_TIME=2;
 public static final int IS_PART_TIME=1;
-public static final int EMP_RATE_PER_HOUR=20;
-public static final int NUM_OF_WORKING_DAYS=20;
-public static final int MAX_HRS_IN_MONTH=100;
-public static int computeEmpWage(){
+public static int computeEmpWage(String Company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth){
 int empHrs=0;
 int totalEmpHrs=0 , totalWorkingDays=0 ;
 System.out.println("Welcome");
-while((totalEmpHrs<=MAX_HRS_IN_MONTH) && (totalWorkingDays<NUM_OF_WORKING_DAYS)){
+while((totalEmpHrs<=maxHoursPerMonth) && (totalWorkingDays<numOfWorkingDays)){
 totalWorkingDays++;
 int check=(int)Math.floor(Math.random()*10 % 3);
 switch (check)
@@ -28,12 +25,13 @@ empHrs=0;
 totalEmpHrs  +=empHrs;
 System.out.println("Day: " + totalWorkingDays + " Emp Hr: "+empHrs);
 }
-int totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
+int totalEmpWage=totalEmpHrs*empRatePerHour;
 
-System.out.println("Total Emp Wage: " + totalEmpWage);
+System.out.println("Total Emp Wage for Company: "+Company+" is: " + totalEmpWage);
 return totalEmpWage;
 }
 public static void main(String[] args){
-computeEmpWage();
+computeEmpWage("DMart",20,2,10);
+computeEmpWage("Reliance",10,4,20);
 }
 }
