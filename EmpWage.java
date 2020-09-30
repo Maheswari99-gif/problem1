e21636caf5188044fe263259d7ed764ad172e62f
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-09-30 15:34:23 +0530

    usecase14

--- EmpWage.java
+++ EmpWage.java
@@ -1,16 +1,19 @@
-
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.*;
 public class EmpWage implements IComputeEmpWage{
 public static final int IS_FULL_TIME=2;
 public static final int IS_PART_TIME=1;
 private int numOfCompany =0;
-private ArrayList<CompanyEmpWage> companyEmpWageList;
+private LinkedList<CompanyEmpWage> companyEmpWageList;
+private Map<String,CompanyEmpWage> companyToEmpWageMap;
 public EmpWage(){
-companyEmpWageList=new ArrayList<>();
+companyEmpWageList=new LinkedList<>();
+companyToEmpWageMap= new HashMap<>();
 }
 public void addCompanyEmpWage(String Company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth){
 CompanyEmpWage companyEmpWage=new CompanyEmpWage( Company, empRatePerHour, numOfWorkingDays,  maxHoursPerMonth);
 companyEmpWageList.add(companyEmpWage);
+companyToEmpWageMap.put(Company,companyEmpWage);
 }
 public void computeEmpWage(){
 for(int i=0;i<companyEmpWageList.size();i++){
@@ -18,6 +21,10 @@ CompanyEmpWage companyEmpWage = companyEmpWageList.get(i);
 companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
 System.out.println(companyEmpWage);
 }
+}
+@Override
+public int getTotalWage(String Company){
+return companyToEmpWageMap.get(Company).totalEmpWage;
 }
 public int computeEmpWage(CompanyEmpWage companyEmpWage){
 int empHrs=0;
@@ -50,5 +57,6 @@ IComputeEmpWage empWage = new EmpWage();
 empWage.addCompanyEmpWage("DMart",20,2,10);
 empWage.addCompanyEmpWage("Reliance",10,4,20);
 empWage.computeEmpWage();
+System.out.println("Total Wage: "+empWage.getTotalWage("DMart"));
 }
 }
--- IComputeEmpWage.java
+++ IComputeEmpWage.java
@@ -1,5 +1,6 @@
 public interface IComputeEmpWage{
 public void addCompanyEmpWage(String Company, int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth);
 public void computeEmpWage();
+public int getTotalWage(String Company);
 
 }
