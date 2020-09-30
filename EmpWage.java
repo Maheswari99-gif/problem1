1ed88533cd0f83bce32b8bf45d6ce1f23d42d075
Maheswari99-gif <maheswarikoyyani999@gmail.com>
2020-09-30 14:07:25 +0530

    usecase12

--- EmpWage.java
+++ EmpWage.java
@@ -1,19 +1,21 @@
-public class EmpWage{
+import java.util.ArrayList;
+public class EmpWage implements IComputeEmpWage{
 public static final int IS_FULL_TIME=2;
 public static final int IS_PART_TIME=1;
 private int numOfCompany =0;
-private CompanyEmpWage[] companyEmpWageArray;
+private ArrayList<CompanyEmpWage> companyEmpWageList;
 public EmpWage(){
-companyEmpWageArray=new CompanyEmpWage[5];
+companyEmpWageList=new ArrayList<>();
 }
 public void addCompanyEmpWage(String Company,int empRatePerHour,int numOfWorkingDays,int maxHoursPerMonth){
-companyEmpWageArray[numOfCompany]=new CompanyEmpWage( Company, empRatePerHour, numOfWorkingDays,  maxHoursPerMonth);
-numOfCompany++;
+CompanyEmpWage companyEmpWage=new CompanyEmpWage( Company, empRatePerHour, numOfWorkingDays,  maxHoursPerMonth);
+companyEmpWageList.add(companyEmpWage);
 }
 public void computeEmpWage(){
-for(int i=0;i<numOfCompany;i++){
-companyEmpWageArray[i].setTotalEmpWage(this.computeEmpWage(companyEmpWageArray[i]));
-System.out.println(companyEmpWageArray[i]);
+for(int i=0;i<companyEmpWageList.size();i++){
+CompanyEmpWage companyEmpWage = companyEmpWageList.get(i);
+companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+System.out.println(companyEmpWage);
 }
 }
 public int computeEmpWage(CompanyEmpWage companyEmpWage){
@@ -28,13 +30,10 @@ switch (check)
 case IS_FULL_TIME:
 empHrs=8;
 break;
-
 case IS_PART_TIME:
 empHrs=4;
 break;
-
 default:
-
 empHrs=0;
 }
 totalEmpHrs  +=empHrs;
@@ -42,10 +41,11 @@ System.out.println("Day: " + totalWorkingDays + " Emp Hr: "+empHrs);
 }
 return totalEmpHrs*companyEmpWage.empRatePerHour;
 }
+
 public static void main(String[] args){
-EmpWage empwage = new EmpWage();
-empwage.addCompanyEmpWage("DMart",20,2,10);
-empwage.addCompanyEmpWage("Reliance",10,4,20);
-empwage.computeEmpWage();
+IComputeEmpWage empWage = new EmpWage();
+empWage.addCompanyEmpWage("DMart",20,2,10);
+empWage.addCompanyEmpWage("Reliance",10,4,20);
+empWage.computeEmpWage();
 }
 }
